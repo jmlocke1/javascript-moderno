a66c8dca1163e65e7bd67d735c9e0206301495db
José Miguel Izquierdo <jmizquierdomar@gmail.com>
2025-12-02 16:43:04 +0100

    Completado capítulo 1

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,16 @@
+// const nombre = prompt("Cual es tu nombre");
+// document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+console.time('Hola');
+
+console.warn('Eso no está permitido')
+console.warn('Eso no está permitido')
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+
+console.timeEnd('Hola');
