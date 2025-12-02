4428f621cbcb909a4c42eb0819fea4cf14bb54f6
José Miguel Izquierdo <jmizquierdomar@gmail.com>
2025-12-02 17:16:46 +0100

    Finalizado capítulo 2

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,25 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = "Monitor de 27 pulgadas";
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinámico, no se especifica tipo de datos
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin valor y asignarlo después
+var disponible;
+disponible = true;
+console.log(disponible);
+disponible = false;
+console.log(disponible);
+
+// Inicializar múltiples variables
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+//  
---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,7 @@
+const producto = 'Tablet';
+console.log(producto);
+
+const productos = ["Monitor", "Tablet"];
+console.log(productos);
+productos[2] = "Móvil";
+console.log(productos)
