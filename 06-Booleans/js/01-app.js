d2e43d0fab97a4274d0d9b1aba161f74d2cbbd8d
José Miguel Izquierdo <jmizquierdomar@gmail.com>
2025-12-03 18:19:41 +0100

    Terminado el capítulo 6, Booleans

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,13 @@
+const boolean1 = true,
+      boolean2 = false,
+      boolean3 = "true";
+
+// console.log(boolean1);
+// console.log(boolean2);
+
+// console.log(typeof boolean1);
+
+console.log(boolean1 == boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(typeof boolean4);
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const autenticado = true;
+
+// if(autenticado){
+//     console.log("Sí puedes ver Netflix");
+// }else{
+//     console.log('No, no puedes verlo');
+// }
+
+// Operador ternario
+console.log(autenticado ? "Sí puedes ver Netflix" : 'No, no puedes verlo');
