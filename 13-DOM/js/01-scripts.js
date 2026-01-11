f3dc0e45860a03895d15f8740ed103d06849c27a
Josemi <worldcode.info@gmail.com>
2026-01-11 21:59:11 +0100

    Comenzando el capítulo 13, DOM

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,10 @@
+let elemento;
+elemento = document;
+elemento = document.querySelectorAll("*");
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = location.hostname; // domain está en desuso, ahora se usa esta instrucción
+elemento = document.forms;
+
+console.log(elemento);
