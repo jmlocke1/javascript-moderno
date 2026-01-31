fbe75638d7518a342d7632a357dff280197f0ef2
Josemi <worldcode.info@gmail.com>
2026-01-31 23:28:36 +0100

    Implementtado el localStorage en el proyecto Gasto Semanal

---
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -0,0 +1,75 @@
+// Variables y Selectores
+const $formulario = document.querySelector('#agregar-gasto'),
+      $gastoListado = document.querySelector('#gastos ul'),
+      $total = document.querySelector('#total'),
+      $restante = document.querySelector('#restante'),
+      $reset = document.querySelector('button[type="reset"]');
+console.log($reset);
+let presupuesto;
+
+// Eventos
+evenListeners();
+function evenListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    $reset.addEventListener('click', e => {
+        e.preventDefault();
+        localStorage.removeItem('presupuesto');
+        window.location.reload();
+    });
+}
+
+// Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        if( !isNaN(presupuesto) ) {
+            this.presupuesto = Number(presupuesto);
+            this.restante = Number(presupuesto);
+            this.gastos = [];
+            this.save(); // Guardamos el objeto recién creado en LocalStorage
+        } else {
+            this.presupuesto = presupuesto.presupuesto;
+            this.restante = presupuesto.restante;
+            this.gastos = presupuesto.gastos;
+        }
+        
+    }
+
+    save() {
+        // De momento solo se guarda en localStorage
+        localStorage.setItem( 'presupuesto', JSON.stringify(this) );
+    }
+}
+
+class UI {
+    static insertarPresupuesto( cantidad ) {
+        const { presupuesto, restante } = cantidad;
+        $total.textContent = moneda(presupuesto);
+        $restante.textContent = moneda(restante);
+    }
+    
+}
+
+// Instanciar
+// const ui = new UI();
+
+// Funciones
+
+function preguntarPresupuesto() {
+    let presupuestoUsuario = JSON.parse(localStorage.getItem('presupuesto'));
+    
+    if(!presupuestoUsuario){
+        presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
+        
+        if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+            window.location.reload();
+        }
+    }
+    
+    
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    UI.insertarPresupuesto(presupuesto);
+}
+
+function moneda(cantidad) {
+    return new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format(cantidad);
+}
