39b9216c438bd7e83f2cb151310972e7e281a7bc
Josemi <worldcode.info@gmail.com>
2026-02-11 22:20:40 +0100

    Terminado el capítulo 30 - Introducción a IndexedDB

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -1,8 +1,12 @@
-
+let DB;
 
 document.addEventListener('DOMContentLoaded', () => {
     crmDB();
-});
+
+    setTimeout(() => {
+        crearCliente();
+    });
+}, 5000);
 
 function crmDB() {
     // Crear base de datos version 1.0
@@ -16,10 +20,44 @@ function crmDB() {
     // Si se creó bien
     crmDB.onsuccess = function() {
         console.log('¡Base de datos creada!');
+
+        DB = crmDB.result;
     }
 
     // Configuración de la base de datos
-    crmDB.onupgradeneeded = function() {
-        console.log('Este método solo se ejecuta una vez');
+    crmDB.onupgradeneeded = function(e) {
+        const db = e.target.result;
+        const objectStore = db.createObjectStore('crm', {
+            keyPath: 'crm',
+            autoIncrement: true
+        });
+
+        // Definir las columnas
+        objectStore.createIndex('nombre', 'nombre', { unique: false });
+        objectStore.createIndex('email', 'email', { unique: true });
+        objectStore.createIndex('telefono', 'telefono', { unique: false });
+
+        console.log('Columnas Creadas');
     }
+}
+
+function crearCliente() {
+    let transaction = DB.transaction(['crm'], 'readwrite');
+
+    transaction.oncomplete = function() {
+        console.log('Transacción completada');
+    }
+
+    transaction.onerror = function() {
+        console.log('Hubo un error en la transacción');
+    }
+
+    const objectStore = transaction.objectStore('crm');
+    const nuevoCliente = {
+        telefono: 976593252,
+        nombre: 'José Miguel',
+        email: 'jose@jose.com'
+    }
+
+    const peticion = objectStore.add(nuevoCliente);
 }
