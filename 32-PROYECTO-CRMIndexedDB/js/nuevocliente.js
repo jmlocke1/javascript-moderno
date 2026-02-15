0b87d0650b0dc20c41c9a5dd7eda9637d9b364dc
Josemi <worldcode.info@gmail.com>
2026-02-15 21:33:43 +0100

    Terminada la funcionalidad de Agregar Cliente

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -30,8 +30,37 @@
         const empresa = document.querySelector('#empresa').value;
         if( nombre === '' || email === '' || telefono === '' || empresa === '' ) {
             imprimirAlerta('Todos los campos son obligatorios', 'error');
-
             return;
+        }
+
+        // Crear un objeto con la información
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+            id: Date.now()
+        }
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction('crm', 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.add(cliente);
+        transaction.onerror = function(e) {
+            console.error(`Hubo un error al añadir el nuevo cliente: ${e.target.error.message}`);
+            imprimirAlerta(`Hubo un error al añadir el nuevo cliente: ${cliente.nombre}`, 'error');
+        }
+
+        transaction.oncomplete = function() {
+            formulario.reset();
+            console.log('Cliente Agregado');
+            imprimirAlerta('Cliente Agregado Correctamente', 'exito');
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
         }
     }
 
