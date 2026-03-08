6374fa50437bfe8bc35c0b0197561899897bdc00
Josemi <worldcode.info@gmail.com>
2026-03-09 00:15:00 +0100

    Mostrando las fichas de imágenes en pantalla

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,7 +1,7 @@
 const $resultado = document.querySelector('#resultado');
 const $formulario = document.querySelector('#formulario');
 const $terminoBusqueda = document.querySelector('#termino');
-
+import { apiKey, urlApi } from "./config.js";
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario);
@@ -38,6 +38,59 @@ function mostrarAlerta(mensaje) {
 }
 
 function buscarImagenes(termino) {
-    const key = '';
-    const url = '';
+    const url = `${urlApi}?key=${apiKey}&q=${termino}`;
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            mostrarImagenes(resultado.hits);
+        });
+}
+
+function mostrarImagenes(imagenes) {
+    limpiarHTML($resultado);
+
+    // Iterar sobre el array de imágenes y construir el HTML
+    imagenes.forEach(imagen => {
+        const { previewURL, webformatURL, likes, views, largeImageURL, pageURL } = imagen;
+        const titulo = titlePhoto(imagen);
+        $resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${webformatURL}" >
+                    <div class="p-4">
+                        <p class="font-bold">${titulo}</p>
+                        <p class="font-bold">${likes} <span class="font-light"> Me gusta</span> </p>
+                        <p class="font-bold">${views} <span class="font-light"> Veces vista</span> </p>
+                        <a
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer" 
+                            title="Ver imagen: ${titulo}. Se abre en una nueva pestaña"
+                        >
+                            Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+function titlePhoto(imagen) {
+    const { pageURL, id } = imagen;
+    const photoElements = pageURL.split('/');
+    let titulo = photoElements.filter(titulo => titulo.includes(id))[0];
+    // Quitamos el id del título
+    titulo = titulo.replace(id, '');
+    // Sustituimos los guiones altos o bajos por espacios
+    titulo = titulo.replaceAll('-', ' ').trim();
+    titulo = titulo.replaceAll('_', ' ').trim();
+    // Primer carácter en mayúscula
+    titulo = titulo[0].toUpperCase() + titulo.substring(1)
+    return titulo;
+}
+
+function limpiarHTML(elemento) {
+    while(elemento.firstChild) {
+        elemento.removeChild(elemento.firstChild);
+    }
 }
--- 38-PROYECTO-PixabayImagenes/js/configDemo.js
+++ 38-PROYECTO-PixabayImagenes/js/configDemo.js
@@ -1,1 +1,2 @@
 export const apiKey = 'Tu API Key de Pixabay';
+export const urlApi = 'https://pixabay.com/api/';
