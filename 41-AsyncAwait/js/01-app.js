930dfdd24c1914362c41fe171c74ff1ee732702f
Josemi <worldcode.info@gmail.com>
2026-04-01 23:06:01 +0200

    Terminado el capítulo 41 - Async Await

---
+++ 41-AsyncAwait/js/01-app.js
@@ -0,0 +1,10 @@
+console.log(1 + 1);
+
+try {
+    hola();
+} catch (error) {
+    console.error(error);
+}
+
+
+console.log(2 + 2);
---
+++ 41-AsyncAwait/js/02-app.js
@@ -0,0 +1,27 @@
+function descargarClientes() {
+
+    return new Promise((resolve, reject) => {
+        const error = true;
+
+        setTimeout(() => {
+            if(!error) {
+                resolve('El listado de clientes de descargó correctamente');
+            } else {
+                reject('Error en la conexión');
+            }
+        }, 3000);
+    });
+}
+
+// Async await
+async function ejecutar() {
+    try {
+        console.log(1 + 1);
+       const respuesta = await descargarClientes(); 
+       console.log(2 + 2);
+       console.log(respuesta);
+    } catch (error) {
+       console.error(error); 
+    }
+}
+ejecutar();
