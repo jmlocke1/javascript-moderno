a8f5ee2276e73f1396fe261e1589380c561399b6
Josemi <worldcode.info@gmail.com>
2026-04-12 01:51:11 +0200

    Refactorización y finalización de 44-PROYECTO-CRM-CRUD-REST

--- 44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
@@ -4,24 +4,26 @@ import { validarCliente } from "./funciones.js";
 (function() {
     const $formulario = document.querySelector('#formulario');
     document.addEventListener('DOMContentLoaded', async () => {
+        const parametrosURL = new URLSearchParams(window.location.search);
+        const id = parametrosURL.get('id');
+        const cliente = await obtenerCliente(id);
+        mostrarCliente(cliente);
+        $formulario.addEventListener('submit', editado);
+    });
+
+    function mostrarCliente(cliente) {
         const $nombre = document.querySelector('#nombre'),
               $email = document.querySelector('#email'),
               $telefono = document.querySelector('#telefono'),
               $empresa = document.querySelector('#empresa'),
               $id = document.querySelector('#id');
-
-        const parametrosURL = new URLSearchParams(window.location.search);
-        const id = parametrosURL.get('id');
-        const cliente = await obtenerCliente(id);
-        const { nombre, email, telefono, empresa } = cliente;
+        const { id, nombre, email, telefono, empresa } = cliente;
         $nombre.value = nombre;
         $email.value = email;
         $telefono.value = telefono;
         $empresa.value = empresa;
         $id.value = id;
-        $formulario.addEventListener('submit', editado);
-
-    });
+    }
 
     function editado(e) {
         const cliente = validarCliente(e, $formulario);
