8c2fd6242bf59525262cc102b7526004854d208f
Josemi <worldcode.info@gmail.com>
2026-07-10 21:16:45 +0200

    Agregando caché a la PWA

--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -1,8 +1,24 @@
+const nombreCache = 'apv-v1';
+const archivos = [
+    '/',
+    'index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apv.js'
+];
+
 // Cuando se instala el Service Worker
 self.addEventListener('install', e => {
     console.log('Instalado el Service Worker');
 
-    console.log(e);
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('cacheando');
+                cache.addAll(archivos);
+            })
+    )
 });
 
 // Activar el Service worker
@@ -14,4 +30,11 @@ self.addEventListener('activate', e => {
 // Evento fetch para descargar archivos estáticos
 self.addEventListener('fetch', e => {
     console.log('Fetch... ', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then( respuestaCache => {
+                return respuestaCache
+            })
+    )
 });
