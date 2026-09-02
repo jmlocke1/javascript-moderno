b5137b4785eda5e8be70055ecfc6335957f375a5
Josemi <worldcode.info@gmail.com>
2026-09-02 23:45:40 +0200

    Comenzando con el header y el footer

--- 53-AgenciaViajesNode/index.js
+++ 53-AgenciaViajesNode/index.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+// Obtener el año actual
+app.use((req, res, next) => {
+    res.locals.unaVariable = 'Una Nueva Variable';
+    console.log(res.locals);
+    next();
+});
+
 // Definir la carpeta pública
 app.use(express.static('public'));
 
