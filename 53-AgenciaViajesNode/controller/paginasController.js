580c31d1a1891b3749b05be7afdb2a123780d164
Josemi <worldcode.info@gmail.com>
2026-09-19 20:28:03 +0200

    Añadimos viajes al inicio y separamos el código de viajes a bloque_viajes

--- 53-AgenciaViajesNode/controller/paginasController.js
+++ 53-AgenciaViajesNode/controller/paginasController.js
@@ -1,11 +1,19 @@
 import { Viaje } from "../models/Viaje.js";
 import { Testimonial } from "../models/Testimoniales.js";
 
-const paginaInicio = (req, res) => {
-    res.render('inicio', {
-        pagina: 'Inicio',
-        clase: 'home'
-    });
+const paginaInicio = async (req, res) => {
+
+    // Consultar 3 viajes del modelo Viaje
+    try {
+        const viajes = await Viaje.findAll({ limit: 3 });
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const paginaViajes = async (req, res) => {
