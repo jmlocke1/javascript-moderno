658bc03a23d3895317db13d483bfde34ebaa8d7d
Josemi <worldcode.info@gmail.com>
2026-09-20 21:14:59 +0200

    Comenzando con los componentes

--- 54-cotizador-prestamos-react/src/App.jsx
+++ 54-cotizador-prestamos-react/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-
+import { useState } from "react"
+import Header from "./components/header"
 
 function App() {
-    const [count, setCount] = useState(0)
-
+    const [cantidad, setCantidad] = useState(10000);
+    
     return (
-        <>
-            <h1 className='text-4xl font-extrabold text-indigo-200'>Hola Mundo</h1>
-        </>
+        <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+            <Header/>
+
+            <input 
+            type="range"
+            className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+            name="" 
+            id="" />
+        </div>
     )
 }
 
