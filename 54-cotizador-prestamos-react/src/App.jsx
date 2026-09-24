a7026923c23839c24cdb28c7d029fec75d3ad8e2
Josemi <worldcode.info@gmail.com>
2026-09-24 22:45:50 +0200

    Creando un estado y un select para los meses. Comenzando el resumen

--- 54-cotizador-prestamos-react/src/App.jsx
+++ 54-cotizador-prestamos-react/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     const [cantidad, setCantidad] = useState(cantidadInicial);
     const [mostrarAlerta, setMostrarAlerta] = useState(false);
     const [mensajeAlerta, setMensajeAlerta] = useState('');
+    const [meses, setMeses] = useState(6);
     
     function handleChange(e) {
         setCantidad(parseInt(e.target.value));
@@ -75,6 +76,30 @@ function App() {
             <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
                 { formatearDinero(cantidad) }
             </p>
+
+            <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+                Elige un <span className="text-indigo-600">Plazo</span> a pagar
+            </h2>
+
+            <select 
+                className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+                value={meses}
+                onChange={e => setMeses(+e.target.value)}
+            >
+                <option value="6">6 Meses</option>
+                <option value="12">12 Meses</option>
+                <option value="24">24 Meses</option>
+            </select>
+
+            <div className="my-5 space-y-3 bg-gray-100 p-5">
+                <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+                    Resumen <span className="text-indigo-600">de pagos</span>
+                </h2>
+
+                <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+                <p className="text-xl text-gray-500 text-center font-bold">Total a pagar</p>
+                <p className="text-xl text-gray-500 text-center font-bold">Mensuales</p>
+            </div>
         </div>
     )
 }
